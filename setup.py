d5e1d92377744c6996f10aba6e7aa5f55be1a23c
Igor Yakubovsky <igorby8881@gmail.com>
2022-09-14 12:04:43 +0200

    Change mock version in setup.py

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
         "test": [
             "pyfakefs==4.6.3",
             "pytest-cov==3.0.0",
-            "pytest-mock==3.8.2",
+            "pytest-mock==3.6.0",
             "pytest==7.1.2",
             "tox==3.26.0",
         ],
